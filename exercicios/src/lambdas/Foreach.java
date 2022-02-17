93933ff574fc2a696916f8cb94fb91ae8e1bac3f
Marcos Ribeiro <marcossrj90@gmail.com>
2022-02-17 17:54:10 -0300

    File from lesson 176

---
+++ exercicios/src/lambdas/Foreach.java
@@ -0,0 +1,33 @@
+package lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Foreach {
+
+	public static void main(String[] args) {
+		
+		List<String> aprovados = Arrays.asList("Ana", "Bia", "Lia", "Gui");
+		
+		System.out.println("Forma tradicional de ForEach:");
+		
+		for(String nome: aprovados) {
+			System.out.println(nome);
+		}
+
+		System.out.println("\nForma lambda 01:");
+		
+//		aprovados.forEach((nome) -> { System.out.println(nome + "!!!"); }); // versão completa
+//		aprovados.forEach(nome -> { System.out.println(nome + "!!!"); }); // não preciso de parenteses quando tenho apenas um parametro
+		aprovados.forEach(nome -> System.out.println(nome + "!!!")); // como só tenho uma instrução, não preciso das chaves 
+
+		/*
+		 * No exemplo de Method Reference, estou passando cada item percorrido da lista
+		 * como um parâmetro para o método println através de uma referência ao método
+		 */
+		
+		System.out.println("\nMethod Reference:");
+		aprovados.forEach(System.out::println);
+	}
+
+}
