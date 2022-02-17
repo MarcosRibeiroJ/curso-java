33834aa7ef2a68101af1a6630cd2b74c45b4f38e
Marcos Ribeiro <marcossrj90@gmail.com>
2022-02-17 11:45:02 -0300

    Files from lesson 174

--- exercicios/src/lambdas/Calculo.java
+++ exercicios/src/lambdas/Calculo.java
@@ -3,12 +3,21 @@ package lambdas;
 /*
  * Por padrão métodos de interface são publicos e abstratos
  * Acrescentei a notação @FunctionalInterface somente para deixar explicito que essa é uma interface funcional
- * Interfaces funcionais possuem apenas um método
+ * Interfaces funcionais possuem apenas um método abstrato
  * Caso eu acrescente mais um método e tire a notação @FunctionalInterface não poderei mais atribuir funções lambda a essa interface
+ * A não ser nos casos em que tenha um método default ou static
  */
 
 @FunctionalInterface
 public interface Calculo {
 
 	double executar(double a, double b);
+	
+	default String legal() {
+		return "legal";
+	}
+	
+	static String muitoLegal() {
+		return "muito legal";
+	}
 }
--- exercicios/src/lambdas/CalculoTeste2.java
+++ exercicios/src/lambdas/CalculoTeste2.java
@@ -10,12 +10,17 @@ public class CalculoTeste2 {
 		calculo = (x, y) -> x * y;
 		System.out.println(calculo.executar(2, 3));
 		
+		System.out.println(calculo.legal());
+		System.out.println(Calculo.muitoLegal());
+		
 		/*
 		 * Nesse caso, estou implementando um função lambda, que seria armazenar um método dentro de uma variavel
 		 * Esse método está sendo sobrescrito de uma interface
 		 * Eu posso criar uma função lambda usando chaves e nesse caso devo escrever um return da função (usado na linha 7)
 		 * Ou apenas uma sentança que seria o comportamento do método (usado na linha 10)
 		 * Esse comportamento é muito semelhante as arrow functions do javascript
+		 * Também estou imprimindo o resultado de um método default da instancia calculo
+		 * E o resultado de um método static da interface Calculo
 		 */
 
 	}
