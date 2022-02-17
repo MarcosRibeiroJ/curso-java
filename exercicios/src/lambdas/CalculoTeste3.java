60da0131c12ab74c53147b8071e56f66eb3bdc0e
Marcos Ribeiro <marcossrj90@gmail.com>
2022-02-17 16:04:35 -0300

    Files from lesson 175

---
+++ exercicios/src/lambdas/CalculoTeste3.java
@@ -0,0 +1,17 @@
+package lambdas;
+
+import java.util.function.BinaryOperator;
+
+public class CalculoTeste3 {
+
+	public static void main(String[] args) {
+		
+		BinaryOperator<Double> calculo = (x, y) -> { return x + y; };
+		System.out.println(calculo.apply(2.0, 3.0));
+		
+		calculo = (x, y) -> x * y;
+		System.out.println(calculo.apply(2.0, 3.0));
+
+	}
+
+}
