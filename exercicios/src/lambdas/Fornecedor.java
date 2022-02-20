283ca5b99049bc9edea0d91369342926f476bcf8
Marcos Ribeiro <marcossrj90@gmail.com>
2022-02-20 10:19:07 -0300

    File from lesson 183

---
+++ exercicios/src/lambdas/Fornecedor.java
@@ -0,0 +1,21 @@
+package lambdas;
+
+/* Functional Interface Supplier
+ * Não recebe nenhum parametro mas especifico o retorno
+ */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+
+	public static void main(String[] args) {
+		
+		Supplier< List<String> > umaLista = () -> Arrays.asList("Ana", "Bia", "Lia", "Gui");
+		
+		System.out.println(umaLista.get());
+
+	}
+
+}
