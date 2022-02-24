ff724dab11589c79d25f3c2a7aac461ed3b23a80
Marcos Ribeiro <marcossrj90@gmail.com>
2022-02-24 15:08:19 -0300

    Files from lesson 201

---
+++ exercicios/src/streams/FuncionarioDesafioFilter.java
@@ -0,0 +1,14 @@
+package streams;
+
+public class FuncionarioDesafioFilter {
+	
+	final String nome;
+	final String operacao;
+	final String contrato;
+	
+	public FuncionarioDesafioFilter(String nome, String operacao, String contrato) {
+		this.nome = nome;
+		this.operacao = operacao;
+		this.contrato = contrato;
+	}
+}
