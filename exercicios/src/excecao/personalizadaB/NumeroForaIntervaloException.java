bf53c59e4f2c8276598cfc06b5acecaed0b3a0ef
Marcos Ribeiro <marcossrj90@gmail.com>
2022-02-27 11:55:18 -0300

    Files from lesson 220

---
+++ exercicios/src/excecao/personalizadaB/NumeroForaIntervaloException.java
@@ -0,0 +1,16 @@
+package excecao.personalizadaB;
+
+@SuppressWarnings("serial")
+public class NumeroForaIntervaloException extends Exception {
+	
+	private String nomeDoAtributo;
+	
+	public NumeroForaIntervaloException(String nomeDoAtributo) {
+		this.nomeDoAtributo = nomeDoAtributo;
+	}
+	
+	@Override
+	public String getMessage() {
+		return String.format("O atributo '%s' está fora do intervalo.", nomeDoAtributo);
+	}
+}
---
+++ exercicios/src/excecao/personalizadaB/TesteValidacoes.java
@@ -0,0 +1,24 @@
+package excecao.personalizadaB;
+
+import excecao.Aluno;
+
+public class TesteValidacoes {
+
+	public static void main(String[] args) {
+		
+		try {
+			Aluno aluno = new Aluno("Ana", 7);
+			Validar.aluno(aluno);
+			
+			Validar.aluno(null);
+		} catch (StringVaziaException e) {
+			System.out.println(e.getMessage());
+		} catch (NumeroForaIntervaloException | IllegalArgumentException e) { //quando estou tratando mais de uma exceção da mesma forma, posso separa-las dentro do catch usando | (significa OU)
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("Fim");
+
+	}
+
+}
