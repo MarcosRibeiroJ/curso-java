c95e1f858efb81b6c09237327f0b550a13cec996
Marcos Ribeiro <marcossrj90@gmail.com>
2022-03-03 22:29:04 -0300

    File from lesson 229

--- campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -1,5 +1,26 @@
 package br.com.cod3r.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
+	
+	private final int linha;
+	private final int coluna;
+	
+	private boolean aberto;
+	private boolean minado;
+	private boolean marcado;
+	
+	private List<Campo> vizinhos = new ArrayList<Campo>();
+	
+	Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	boolean adicionarVizinho(Campo vizinho) {
+		
+	}
 
 }
