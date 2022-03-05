0cf2522122fa41f35e8dd82562e54bc64c5b1415
Marcos Ribeiro <marcossrj90@gmail.com>
2022-03-05 09:23:47 -0300

    Files from lesson 232

--- campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -3,6 +3,8 @@ package br.com.cod3r.cm.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
 public class Campo {
 	
 	private final int linha;
@@ -37,6 +39,36 @@ public class Campo {
 		} else {
 			return false;
 		}
+	}
+	
+	void alternarMarcacao() {
+		if(!aberto) {
+			marcado = !marcado;
+		}
+	}
+	
+	boolean abrir() {
+		
+		if(!aberto && !marcado) {
+			aberto = true;
+			
+			if(minado) {
+				throw new ExplosaoException();
+			}
+			
+			if(vizinhancaSegura()) {
+				vizinhos.forEach(v -> v.abrir());
+			}
+			
+			return true;
+			
+		} else {
+			return false;
+		}	
+	}
+	
+	boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
 
 }
