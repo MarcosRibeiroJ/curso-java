807b2e0f6497ab0f5b10acea3ec7032e7f32dbf9
Marcos Ribeiro <marcossrj90@gmail.com>
2022-03-07 17:59:17 -0300

    Files from lesson 238

--- campo-minado/src/br/com/cod3r/cm/Aplicacao.java
+++ campo-minado/src/br/com/cod3r/cm/Aplicacao.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.cm;
 
 import br.com.cod3r.cm.modelo.Tabuleiro;
+import br.com.cod3r.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
@@ -8,12 +9,7 @@ public class Aplicacao {
 		
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
 		
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alternarMarcacao(4, 4);
-		tabuleiro.alternarMarcacao(4, 5);
-		
-		System.out.println(tabuleiro);
-
+		new TabuleiroConsole(tabuleiro);
 	}
 
 }
