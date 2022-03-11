942e683c7f70b9af5aadfe3b3d08c3cc0b5a40a5
Marcos Ribeiro <marcossrj90@gmail.com>
2022-03-11 20:46:50 -0300

    Files from lesson 258

--- campo-minado-swing/src/br/com/cod3r/cm/modelo/Campo.java
+++ campo-minado-swing/src/br/com/cod3r/cm/modelo/Campo.java
@@ -13,10 +13,20 @@ public class Campo {
 	private boolean marcado;
 	
 	private List<Campo> vizinhos = new ArrayList<Campo>();
+	private List<CampoObservador> observadores = new ArrayList<CampoObservador>();
 	
 	Campo(int linha, int coluna) {
 		this.linha = linha;
 		this.coluna = coluna;
+	}
+	
+	public void registrarObservador(CampoObservador observador) {
+		observadores.add(observador);
+	}
+	
+	private void notificarObservadores(CampoEvento evento) {
+		observadores.stream()
+			.forEach(o -> o.eventoOcorreu(this, evento));
 	}
 	
 	boolean adicionarVizinho(Campo vizinho) {
@@ -42,18 +52,27 @@ public class Campo {
 	void alternarMarcacao() {
 		if(!aberto) {
 			marcado = !marcado;
+			
+			if(marcado) {
+				notificarObservadores(CampoEvento.MARCAR);
+			} else {
+				notificarObservadores(CampoEvento.DESMARCAR);				
+			}
 		}
 	}
 	
 	boolean abrir() {
 		
 		if(!aberto && !marcado) {
-			aberto = true;
-			
 			if(minado) {
-				// TODO Implementar nova versao
+				notificarObservadores(CampoEvento.EXPLODIR);
+				return true;
 			}
 			
+			setAberto(true);
+			
+			notificarObservadores(CampoEvento.ABRIR);
+
 			if(vizinhancaSegura()) {
 				vizinhos.forEach(v -> v.abrir());
 			}
@@ -83,6 +102,10 @@ public class Campo {
 	
 	void setAberto(boolean aberto) {
 		this.aberto = aberto;
+		
+		if(aberto) {
+			notificarObservadores(CampoEvento.ABRIR);
+		}
 	}
 
 	public boolean isAberto() {
