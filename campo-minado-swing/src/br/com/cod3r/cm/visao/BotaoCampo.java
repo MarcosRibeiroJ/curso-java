8c1077850a1511e9eee16a0f4d34f5c2a225017c
Marcos Ribeiro <marcossrj90@gmail.com>
2022-03-14 21:50:43 -0300

    File from lesson 262

--- campo-minado-swing/src/br/com/cod3r/cm/visao/BotaoCampo.java
+++ campo-minado-swing/src/br/com/cod3r/cm/visao/BotaoCampo.java
@@ -1,13 +1,62 @@
 package br.com.cod3r.cm.visao;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 import br.com.cod3r.cm.modelo.Campo;
+import br.com.cod3r.cm.modelo.CampoEvento;
+import br.com.cod3r.cm.modelo.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton {
+public class BotaoCampo extends JButton implements CampoObservador {
+	
+	private final Color BG_PADRAO = new Color(184, 184, 184);
+	private final Color BG_MARCAR = new Color(8, 179, 247);
+	private final Color BG_EXPLODIR = new Color(189, 66, 68);
+	private final Color TEXTO_VERDE = new Color(0, 100, 0);
+	
+	private Campo campo;
 	
 	public BotaoCampo(Campo campo) {
+		this.campo = campo;
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createBevelBorder(0));
+		
+		campo.registrarObservador(this);
+	}
+
+	@Override
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		switch (evento) {
+		case ABRIR:
+			aplicarEstiloAbrir();
+			break;
+		case MARCAR:
+			aplicarEstiloMarcar();
+			break;
+		case EXPLODIR:
+			aplicarEstiloExplodir();
+			break;
+		default:
+			aplicarEstiloPadrao();
+		}
+	}
+
+	private void aplicarEstiloPadrao() {
+		
+	}
+
+	private void aplicarEstiloExplodir() {
+		
+	}
+
+	private void aplicarEstiloMarcar() {
+		
+	}
+
+	private void aplicarEstiloAbrir() {
 		
 	}
 
