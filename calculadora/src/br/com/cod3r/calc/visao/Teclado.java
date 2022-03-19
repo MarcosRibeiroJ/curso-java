0c1ca68d18c7e60d6a294f9225999cf02ce24ce9
Marcos Ribeiro <marcossrj90@gmail.com>
2022-03-19 15:00:26 -0300

    Files from lesson 269

--- calculadora/src/br/com/cod3r/calc/visao/Teclado.java
+++ calculadora/src/br/com/cod3r/calc/visao/Teclado.java
@@ -1,9 +1,16 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Teclado extends JPanel {
+	
+	private final Color COR_CINZA_ESCURO = new Color(68, 68, 68);
+	private final Color COR_CINZA_CLARO = new Color(99, 99, 99);
+	private final Color COR_LARANJA = new Color(242, 163, 60);
 	
 	public Teclado() {
 		
