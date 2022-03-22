8ccde7292070f2e17ade5db19872b60b8345e461
Marcos Ribeiro <marcossrj90@gmail.com>
2022-03-22 20:23:55 -0300

    Files from lesson 277 to 278

--- calculadora/src/br/com/cod3r/calc/visao/Teclado.java
+++ calculadora/src/br/com/cod3r/calc/visao/Teclado.java
@@ -30,40 +30,41 @@ public class Teclado extends JPanel implements ActionListener {
 		c.fill = GridBagConstraints.BOTH;
 		
 		//Linha 1
-		c.gridwidth = 3;
-		adicionarBota("AC", COR_CINZA_ESCURO, c, 0, 0);
+		c.gridwidth = 2;
+		adicionarBotao("AC", COR_CINZA_ESCURO, c, 0, 0);
 		c.gridwidth = 1;
-		adicionarBota("/", COR_LARANJA, c, 3, 0);
+		adicionarBotao("±", COR_CINZA_ESCURO, c, 2, 0);
+		adicionarBotao("/", COR_LARANJA, c, 3, 0);
 
 		//Linha 2
-		adicionarBota("7", COR_CINZA_CLARO, c, 0, 1);
-		adicionarBota("8", COR_CINZA_CLARO, c, 1, 1);
-		adicionarBota("9", COR_CINZA_CLARO, c, 2, 1);
-		adicionarBota("*", COR_LARANJA, c, 3, 1);
+		adicionarBotao("7", COR_CINZA_CLARO, c, 0, 1);
+		adicionarBotao("8", COR_CINZA_CLARO, c, 1, 1);
+		adicionarBotao("9", COR_CINZA_CLARO, c, 2, 1);
+		adicionarBotao("*", COR_LARANJA, c, 3, 1);
 		
 		//Linha 3
-		adicionarBota("4", COR_CINZA_CLARO, c, 0, 2);
-		adicionarBota("5", COR_CINZA_CLARO, c, 1, 2);
-		adicionarBota("6", COR_CINZA_CLARO, c, 2, 2);
-		adicionarBota("-", COR_LARANJA, c, 3, 2);
+		adicionarBotao("4", COR_CINZA_CLARO, c, 0, 2);
+		adicionarBotao("5", COR_CINZA_CLARO, c, 1, 2);
+		adicionarBotao("6", COR_CINZA_CLARO, c, 2, 2);
+		adicionarBotao("-", COR_LARANJA, c, 3, 2);
 		
 		//Linha 4
-		adicionarBota("1", COR_CINZA_CLARO, c, 0, 3);
-		adicionarBota("2", COR_CINZA_CLARO, c, 1, 3);
-		adicionarBota("3", COR_CINZA_CLARO, c, 2, 3);
-		adicionarBota("+", COR_LARANJA, c, 3, 3);
+		adicionarBotao("1", COR_CINZA_CLARO, c, 0, 3);
+		adicionarBotao("2", COR_CINZA_CLARO, c, 1, 3);
+		adicionarBotao("3", COR_CINZA_CLARO, c, 2, 3);
+		adicionarBotao("+", COR_LARANJA, c, 3, 3);
 		
 		//Linha 5
 		c.gridwidth = 2;
-		adicionarBota("0", COR_CINZA_CLARO, c, 0, 4);
+		adicionarBotao("0", COR_CINZA_CLARO, c, 0, 4);
 		c.gridwidth = 1;
-		adicionarBota(",", COR_CINZA_CLARO, c, 2, 4);
-		adicionarBota("=", COR_LARANJA, c, 3, 4);
+		adicionarBotao(",", COR_CINZA_CLARO, c, 2, 4);
+		adicionarBotao("=", COR_LARANJA, c, 3, 4);
 		
 				
 	}
 
-	private void adicionarBota(String texto, Color cor, GridBagConstraints c, int x, int y) {
+	private void adicionarBotao(String texto, Color cor, GridBagConstraints c, int x, int y) {
 		c.gridx = x;
 		c.gridy = y;
 		Botao botao = new Botao(texto, cor);
