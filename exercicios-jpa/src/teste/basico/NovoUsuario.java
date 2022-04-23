6707d2f56b99b8b052de8a8085981c64ef39e3b5
Marcos Ribeiro <marcossrj90@gmail.com>
2022-04-23 16:49:41 -0300

    Files from lesson 331

---
+++ exercicios-jpa/src/teste/basico/NovoUsuario.java
@@ -0,0 +1,42 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class NovoUsuario {
+
+	public static void main(String[] args) {
+		
+		/* EntityManagerFactory
+		 * responsavel por gerenciar as EntityManager
+		 * Devo instanciar uma classe Persistence e passo como parametro para o método createEntityManagerFactory
+		 * a unidade de persistencia definida no arquivo persistence.xml
+		 */
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+		
+		/* EntityManager
+		 * responsável pelo CRUD de uma entidade (classe) mapeada para ser utilizada em banco de dados
+		 * Também possue internamente informações para conexão ao banco
+		 */
+		EntityManager em = emf.createEntityManager();
+		
+		/* Inserir informações no banco
+		 * Para inserir dados no banco, devo utilizar o método persist
+		 * Passando como parametro um objeto de classe mapeada
+		 */
+		
+		Usuario novoUsuario = new Usuario("Carlos", "carlos@testeJPA.com");
+		
+		em.getTransaction().begin(); //inicia uma transação com BD
+		em.persist(novoUsuario); //insere os dados de novo usuario no banco (INSERT do SQL)
+		em.getTransaction().commit(); //commit das alterações
+		
+		em.close();
+		emf.close();
+
+	}
+
+}
