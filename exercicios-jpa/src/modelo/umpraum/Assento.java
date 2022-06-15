bf010d418247371e8c3e2cd08c3edf1d5080379a
Marcos de Souza Ribeiro Junior <marcossrj90@gmail.com>
2022-06-15 19:49:09 -0300

    Files from lesson 345

--- exercicios-jpa/src/modelo/umpraum/Assento.java
+++ exercicios-jpa/src/modelo/umpraum/Assento.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -14,6 +15,8 @@ public class Assento {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	@OneToOne(mappedBy = "assento")
+	private Cliente cliente;
 	
 	Assento() {
 		
@@ -38,4 +41,13 @@ public class Assento {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
 }
