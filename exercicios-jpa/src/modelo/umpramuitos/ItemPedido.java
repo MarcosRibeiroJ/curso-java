1ee6df28e6e61bb12f155e13ea2002715893261d
Marcos de Souza Ribeiro Junior <marcossrj90@gmail.com>
2022-06-16 19:19:40 -0300

    Files from lesson 348

--- exercicios-jpa/src/modelo/umpramuitos/ItemPedido.java
+++ exercicios-jpa/src/modelo/umpramuitos/ItemPedido.java
@@ -2,6 +2,7 @@ package modelo.umpramuitos;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,7 +21,7 @@ public class ItemPedido {
 	@ManyToOne
 	private Pedido pedido;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)
 	private Produto produto;
 	
 	@Column(nullable = false)
